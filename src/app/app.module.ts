aea36ce55c93fad75f4b356228953bcf2a61e0e1
Shubham Kadam <1shubhamkadam1@gmail.com>
2023-01-23 10:11:17 +0530

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,12 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
       serverLoggingUrl: '/api/logs',
       level: NgxLoggerLevel.OFF,
       serverLogLevel: NgxLoggerLevel.ERROR,
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [],
